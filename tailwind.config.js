97f7ad8f0994e0bb558466717c8a40de5a99e6d9
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-03-24 22:48:22 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'gray': {
+          900 : '#13131A',
+          800 : '#16161F',
+          700 : '#1C1C27',
+          600 : '#22222F',
+          500 : '#3B3B54',
+          400 : '#7F7F98',
+          300 : '#ABABC4',
+          200 : '#BFBFD4',
+          100 : '#FAFAFA',
+        },
+        'blue-light' : '#8FB2F5',
+      },
+      fontFamily : {
+        'nunito' : '"Nunito", sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}
+
