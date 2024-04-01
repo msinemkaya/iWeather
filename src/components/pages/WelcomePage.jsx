c89775b1e18b4c61351909e4686dda686c996ffa
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-04-01 18:05:58 +0300

    feat: stored longitude and latitude of chosen location by user

--- src/components/pages/WelcomePage.jsx
+++ src/components/pages/WelcomePage.jsx
@@ -3,23 +3,28 @@ import { Input } from '../base/Input.jsx'
 import { useState } from 'react'
 import { LoadScript, StandaloneSearchBox } from '@react-google-maps/api'
 
+const libs = [ 'places' ]
 export const WelcomePage = () => {
   const [ autocomplete, setAutocomplete ] = useState(null)
   const [ place, setPlace ] = useState('')
+  const [location, setLocation] = useState([])
 
   const handlePlaceChanged = () => {
     if (autocomplete !== null) {
-      const place = autocomplete.getPlace()
-      setPlace(place.formatted_address)
+      const places = autocomplete.getPlaces()
+      const selected = places.length > 0 ? places[0].formatted_address : ''
+      setPlace(selected)
+      setLocation([places[0].geometry.location.lng(), places[0].geometry.location.lat()])
+    console.log()
     } else {
       console.log('Not loaded yet!')
     }
   }
-
-  const handleChange = ({ target }) => {
-    setPlace(target.value)
+  const handleChange = (e) => {
+    setPlace(e.target.value)
   }
 
+  console.log(place)
   return (
     <>
       <div className='my-12 grid w-full fixed place-items-center'>
@@ -33,7 +38,7 @@ export const WelcomePage = () => {
         </div>
 
         <div className='mx-8'>
-          <LoadScript googleMapsApiKey={import.meta.env.VITE_LOCATION_API_KEY} libraries={[ 'places' ]}>
+          <LoadScript googleMapsApiKey={import.meta.env.VITE_LOCATION_API_KEY} libraries={libs}>
             <StandaloneSearchBox onLoad={setAutocomplete} onPlacesChanged={handlePlaceChanged}>
               <Input type='text' placeholder='Search location' onChange={handleChange} value={place}/>
             </StandaloneSearchBox>
