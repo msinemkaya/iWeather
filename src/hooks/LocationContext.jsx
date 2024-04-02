ea82ac39e5efdf2c7f7ab4f8b959f8c38b2133e7
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-04-02 22:52:18 +0300

    feat: updated location context

--- src/hooks/LocationContext.jsx
+++ src/hooks/LocationContext.jsx
@@ -1,25 +1,29 @@
 import { createContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useWeather } from './use-weather.js'
 
 export const LocationContext = createContext({})
+
 export const LocationContextProvider = ({ children }) => {
   const [ autocomplete, setAutocomplete ] = useState(null)
   const [ place, setPlace ] = useState('')
   const [ location, setLocation ] = useState([])
+  const { fetchWeatherData } = useWeather()
   const navigate = useNavigate()
 
-  const navigateToWeatherPage = (lang, lat) => {
-    navigate(`/weather/${lang}/${lat}`)
+  const navigateToWeatherPage = (long, lat) => {
+    navigate(`/weather/${long}/${lat}`)
   }
 
-  const handlePlaceChanged = () => {
+  const handlePlaceChanged = async () => {
     if (autocomplete !== null) {
       const places = autocomplete.getPlaces()
       const selected = places.length > 0 ? places[0].formatted_address : ''
-      const long = places[0].geometry.location.lng()
-      const lat = places[0].geometry.location.lat()
+      const long = places[0].geometry.location.lng().toFixed(2)
+      const lat = places[0].geometry.location.lat().toFixed(2)
       setPlace(selected)
       setLocation([ long, lat ])
+      await fetchWeatherData(lat, long)
       navigateToWeatherPage(long, lat)
     } else {
       console.log('Not loaded yet!')
