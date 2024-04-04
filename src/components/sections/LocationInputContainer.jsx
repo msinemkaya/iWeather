2aae0e916cf42188e2c1fc23f32a0f6c627cc856
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-04-04 15:41:46 +0300

    fix: fixed isLoaded control

--- src/components/sections/LocationInputContainer.jsx
+++ src/components/sections/LocationInputContainer.jsx
@@ -3,8 +3,9 @@ import { useMemo } from 'react'
 
 export const LocationInputContainer = ({ children }) => {
   const libs = useMemo(() => [ 'places' ], [])
-  const isLoaded = window.google === undefined
-  return isLoaded ? (
+  const isLoaded = window.google || false
+
+  return !isLoaded ? (
     <LoadScript googleMapsApiKey={import.meta.env.VITE_LOCATION_API_KEY} libraries={libs}>
       {children}
     </LoadScript>) : (
