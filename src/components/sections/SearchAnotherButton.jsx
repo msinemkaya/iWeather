eb0f08987b7b9d08bd4a602e8212a3787246de23
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-04-09 17:23:32 +0300

    fix: removed empty line and refactored

--- src/components/sections/SearchAnotherButton.jsx
+++ src/components/sections/SearchAnotherButton.jsx
@@ -4,10 +4,10 @@ import { useNavigate } from 'react-router-dom'
 export const SearchAnotherButton = () => {
   const navigate = useNavigate()
 
-
   return (
     <Card>
-      <button onClick={() => navigate('/')} className='text-gray-200 flex items-center capitalize justify-evenly heading sm w-full p-4'>
+      <button onClick={() => navigate('/')}
+              className='text-gray-200 flex items-center capitalize justify-evenly heading sm w-full p-4'>
         <span>Search for another place</span>
         <span>
             <img src='/src/assets/logo.svg' alt=''/>
