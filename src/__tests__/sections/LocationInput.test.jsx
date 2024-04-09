436d34b8e7e1389c08050d86e93361ba6769fa8d
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-04-09 17:19:45 +0300

    feat: created test for LocationInput component

---
+++ src/__tests__/sections/LocationInput.test.jsx
@@ -0,0 +1,24 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { LocationInput } from '../../components/sections/LocationInput.jsx'
+import { initialize } from '@googlemaps/jest-mocks'
+import '@testing-library/jest-dom'
+
+describe('LocationInput', () => {
+  beforeEach(() => {
+    initialize()
+  })
+
+  it('renders the search input', () => {
+    render(<LocationInput/>)
+    const searchInput = screen.getByPlaceholderText('Search location')
+    expect(searchInput).toBeInTheDocument()
+  })
+
+  it('updates the input value when typed', () => {
+    render(<LocationInput/>)
+
+    const searchInput = screen.getByPlaceholderText('Search location')
+    fireEvent.change(searchInput, { target: { value: 'Opole' } })
+    expect(searchInput.value).toBe('Opole')
+  })
+})
