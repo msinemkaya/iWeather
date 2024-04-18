285d901bb97eb7097274b7c99b5ab9f45b388632
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-04-19 02:38:47 +0300

    feat: created fetchWeatherData test and refactored usage of env for testability

---
+++ src/__tests__/utils/fetchWeatherData.test.js
@@ -0,0 +1,49 @@
+import { fetchWeatherData } from '../../utils/fetchWeatherData.js'
+import { weatherApi } from '../../utils/weatherRequestInstance.js'
+import { mockFetchWeatherData } from '../../__mocks__/fetchWeatherDataData.js'
+
+jest.mock('../../utils/weatherRequestInstance.js', () => ({
+  weatherApi: {
+    get: jest.fn(),
+  },
+}))
+
+describe('fetchWeatherData function', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should fetch weather data successfully', async () => {
+    const lat = 40.71
+    const lon = -74.00
+
+    const infoData = mockFetchWeatherData.responseInfoData
+    const weeklyData = mockFetchWeatherData.responseWeeklyData
+
+    weatherApi.get.mockImplementationOnce(() => Promise.resolve({ data: infoData }))
+    weatherApi.get.mockImplementationOnce(() => Promise.resolve({ data: weeklyData }))
+
+    const result = await fetchWeatherData(lat, lon)
+
+    expect(weatherApi.get).toHaveBeenCalledTimes(2)
+    expect(weatherApi.get).toHaveBeenCalledWith('/', { params: { lat, lon } })
+    expect(weatherApi.get).toHaveBeenCalledWith('/daily', { params: { lat, lon } })
+    expect(result).toEqual({ responseInfoData: infoData, responseWeeklyData: weeklyData })
+  })
+
+  it('should handle errors', async () => {
+    const lat = 40.71
+    const lon = -74.00
+
+    const errorMessage = 'Failed to fetch weather data'
+
+    weatherApi.get.mockRejectedValueOnce(new Error(errorMessage))
+
+    const result = await fetchWeatherData(lat, lon)
+
+    expect(weatherApi.get).toHaveBeenCalledTimes(2)
+    expect(weatherApi.get).toHaveBeenCalledWith('/', { params: { lat, lon } })
+    expect(weatherApi.get).toHaveBeenCalledWith('/daily', { params: { lat, lon } })
+    expect(result).toEqual(new Error(errorMessage))
+  })
+})
--- src/utils/weatherRequestInstance.js
+++ src/utils/weatherRequestInstance.js
@@ -4,7 +4,7 @@ export const weatherApi = axios.create({
   baseURL: 'https://api.openweathermap.org/data/2.5/forecast',
   params: {
     cnt: 7,
-    appid: import.meta.env.VITE_WEATHER_API_KEY,
+    appid: process.env.VITE_WEATHER_API_KEY,
     units: 'metric',
   },
 })
