5062ebcda3eb8e4a1caab84306e123f78a24ae3a
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-04-21 04:18:42 +0300

    feat: added some styling

--- src/components/sections/SearchAnotherButton.jsx
+++ src/components/sections/SearchAnotherButton.jsx
@@ -1,13 +1,14 @@
 import { Card } from '../base/Card.jsx'
 import { useNavigate } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
+
 export const SearchAnotherButton = () => {
   const navigate = useNavigate()
 
   return (
-    <Card data-testid='search-another-button' className=>
+    <Card data-testid='search-another-button'>
       <button onClick={() => navigate('/')}
-              className='text-gray-200 flex items-center capitalize justify-evenly heading sm w-full p-4'>
+              className='text-gray-200 flex items-center capitalize justify-evenly sm:justify-center sm:gap-4 heading sm w-full p-4'>
         <span>Search for another place</span>
         <span>
             <img src={logo} alt=''/>
